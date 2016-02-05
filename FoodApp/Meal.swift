f4506442a4477aed77fa427e615e165e4e85e74c
Emmanuel Cuevas <emmanuelcuevas@Emmanuels-MacBook-Pro.local>
2016-02-05 16:05:10 -0600

    first commit

---
+++ FoodApp/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  FoodApp
+//
+//  Created by Emmanuel Cuevas on 2/5/16.
+//  Copyright © 2016 Emmanuel Cuevas. All rights reserved.
+//
+
+import Foundation
---
+++ FoodApp/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodApp
+//
+//  Created by Emmanuel Cuevas on 2/5/16.
+//  Copyright © 2016 Emmanuel Cuevas. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
